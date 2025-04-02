59bbdb4d24fc0eef12df155eff69887949a99361
SyrineZaier <Syrine.zaier@esprit.tn>
2025-04-02 14:58:13 +0200

    Crud + Testing

---
+++ BackEnd/src/main/java/com/example/backend/controllers/LeaveController.java
@@ -0,0 +1,42 @@
+package com.example.backend.controllers;
+
+
+import com.example.backend.entities.Leave;
+import com.example.backend.services.ILeaveService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/leave")
+@CrossOrigin(origins = "http://localhost:4200")
+public class LeaveController {
+
+
+    ILeaveService leaveService;
+    @GetMapping("/retrieve-all-leave")
+    public List<Leave> getLeaves() {
+        List<Leave> listLeaves = leaveService.allLeaves();
+        return listLeaves;
+    }
+
+
+    @PostMapping("/add-leave")
+    public Leave addLeave(@RequestBody Leave l) {
+        Leave leave = leaveService.addLeave(l);
+        return leave;
+    }
+    @DeleteMapping("/remove-leave/{leave-id}")
+    public void removeLeave(@PathVariable("leave-id") int Id) {
+        leaveService.deleteLeave(Id);
+    }
+
+    @PutMapping("/modify-Leave")
+    public Leave modifyLeave(@RequestBody Leave l) {
+        Leave leave = leaveService.updateLeave(l);
+        return leave;
+    }
+
+}
---
+++ BackEnd/src/main/java/com/example/backend/services/LeaveService.java
@@ -0,0 +1,35 @@
+package com.example.backend.services;
+
+import com.example.backend.entities.Leave;
+import com.example.backend.repositories.LeaveRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+@AllArgsConstructor
+
+public class LeaveService implements ILeaveService{
+    LeaveRepository leaveRepository;
+
+    @Override
+    public Leave addLeave(Leave leave) {
+        return leaveRepository.save(leave);
+    }
+
+    @Override
+    public void deleteLeave(int idLeave) {
+
+        leaveRepository.deleteById(idLeave);
+    }
+
+    @Override
+    public Leave updateLeave(Leave leave) {
+        return leaveRepository.save(leave);
+    }
+
+    @Override
+    public List<Leave> allLeaves() {
+        return leaveRepository.findAll();
+    }
+}
