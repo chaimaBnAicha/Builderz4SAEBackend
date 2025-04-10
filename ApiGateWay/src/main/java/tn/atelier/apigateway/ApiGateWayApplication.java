aee3bd03b16f457b9cfe59d81d96ddc196a0c7d3
HasnaouiOumayma <oumayma.hasnaoui@esprit.tn>
2025-04-10 22:50:57 +0200

    apigateway

---
+++ ApiGateWay/src/main/java/tn/atelier/apigateway/ApiGateWayApplication.java
@@ -0,0 +1,36 @@
+package tn.atelier.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
+import org.springframework.http.HttpMethod;
+import java.time.Duration;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ApiGateWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGateWayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("tache-etape-service", r -> r
+                        .path("/api/**")
+                        .filters(f -> f
+                            .preserveHostHeader()
+                            .retry(retryConfig -> retryConfig
+                                .setRetries(3)
+                                .setMethods(HttpMethod.POST, HttpMethod.GET))
+                            .rewritePath("/api/(?<segment>.*)", "/api/${segment}"))
+                        .uri("lb://tache-etape"))
+                // .uri("http://localhost:8081"))
+                .build();
+    }
+}
