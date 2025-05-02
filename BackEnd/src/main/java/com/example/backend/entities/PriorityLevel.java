fe9e93d036d4685712aa6534a56b35d99ac3d301
chaima154 <116753982+chaima154@users.noreply.github.com>
2025-05-02 22:57:46 +0100

    DeletedFiles

--- BackEnd/src/main/java/com/example/backend/entities/PriorityLevel.java
+++
@@ -1,5 +0,0 @@
-package com.example.backend.entities;
-
-public enum PriorityLevel {
-    LOW, MEDIUM, HIGH
-}
--- BackEnd/src/main/java/com/example/backend/entities/Statut.java
+++
@@ -1,5 +0,0 @@
-package com.example.backend.entities;
-
-public enum Statut {
-    Pending, Approved, Rejected
-}
